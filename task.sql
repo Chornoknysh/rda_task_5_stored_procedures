83403e681c60952c5e1290080bcb82dfa0975c69
Chornoknysh <danuk123boss@gmail.com>
2025-09-10 18:30:34 +0200

    solution

--- task.sql
+++ task.sql
@@ -1,16 +1,13 @@
+DROP DATABASE IF EXISTS ShopDB;
+CREATE DATABASE ShopDB;
 USE ShopDB;
 
 DELIMITER $$
 
--- Додаємо guard, щоб можна було безпечно перезапускати скрипт
 DROP PROCEDURE IF EXISTS get_warehouse_product_inventory;
 
 CREATE PROCEDURE get_warehouse_product_inventory(IN inWarehouseID INT)
 BEGIN
-    /*
-      Повертає список продуктів і їх кількість
-      для конкретного складу (WarehouseID)
-    */
     SELECT
         p.Name AS ProductName,
         pi.WarehouseAmount AS Amount
